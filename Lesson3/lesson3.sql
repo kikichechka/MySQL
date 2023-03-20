fee179dd849f29e32aab89677ba73baac9e42c32
kikichechka <90552053+kikichechka@users.noreply.github.com>
2023-03-20 22:58:21 +0300

    third home work

---
+++ Lesson3/lesson3.sql
@@ -0,0 +1,80 @@
+DROP DATABASE IF EXISTS lesson3;
+
+CREATE DATABASE IF NOT EXISTS lesson3;
+
+USE lesson3;
+
+CREATE TABLE IF NOT EXISTS staff
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `first_name` VARCHAR(45),
+    `last_name` VARCHAR(45),
+    `post` VARCHAR(45),
+    `seniority` INT,
+	`salary` INT,
+    `age` INT
+);
+
+USE lesson3;
+SELECT * FROM staff;
+-- TRUNCATE staff; -- очищение таблицы
+INSERT staff (`first_name`, `last_name`, `post`, `seniority`, `salary`, `age`)
+VALUES
+ ('Вася', 'Петров', 'Начальник', 40, 100000, 60),
+ ('Петр', 'Власов', 'Начальник', 8, 70000, 30),
+ ('Катя', 'Катина', 'Инженер', 2, 70000, 25),
+ ('Саша', 'Сасин', 'Инженер', 12, 50000, 35),
+ ('Иван', 'Петров', 'Рабочий', 40, 30000, 59),
+ ('Петр', 'Петров', 'Рабочий', 20, 55000, 40),
+ ('Сидр', 'Сидоров', 'Рабочий', 10, 20000, 35),
+ ('Антон', 'Антонов', 'Рабочий', 8, 19000, 28),
+ ('Юрий', 'Юрков', 'Рабочий', 5, 15000, 25),
+ ('Максим', 'Петров', 'Рабочий', 2, 11000, 22),
+ ('Юрий', 'Петров', 'Рабочий', 3, 12000, 24),
+ ('Людмила', 'Маркина', 'Уборщик', 10, 10000, 49);
+ 
+ SELECT * FROM staff;
+ 
+ -- Отсортируйте данные по полю заработная плата (salary) в порядке: возрастания
+SELECT *
+FROM staff
+ORDER BY salary ; 
+
+ -- Отсортируйте данные по полю заработная плата (salary) в порядке: убывания
+SELECT *
+FROM staff
+ORDER BY salary DESC ; 
+
+-- Выведите 5 максимальных заработных плат (saraly)
+SELECT *
+FROM staff
+LIMIT 5;
+
+-- Посчитайте суммарную зарплату (salary) по каждой специальности (роst)
+SELECT 
+	post,
+	SUM(salary) AS "Суммарная ЗП сотрудников"
+FROM staff
+GROUP BY post;
+
+-- Найдите кол-во сотрудников с специальностью (post) «Рабочий» в возрасте от 24 до 49 лет включительно.
+SELECT 
+	post, 
+	COUNT(post) AS "Количество сотрудников в возрасте от 24 до 49 лет"
+FROM staff
+WHERE post = 'Рабочий' AND age BETWEEN 24 AND 49
+GROUP BY post;
+
+-- Найдите количество специальностей
+SELECT 
+	post, 
+	COUNT(post) AS "Количество специальностей"
+FROM staff
+GROUP BY post;
+
+-- Выведите специальности, у которых средний возраст сотрудников меньше 30 лет включительно.
+SELECT
+    post
+FROM staff
+GROUP BY post
+HAVING COUNT(post) > 1 AND AVG(age) <= 30;
